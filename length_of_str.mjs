4cb088f988bdd975ffe4c19e0b10ccf7f21995e5
iiingkeep <yeonjin1939@gmail.com>
2024-10-23 23:06:31 +0900

    length_of_str file upload

---
+++ length_of_str.mjs
@@ -0,0 +1,9 @@
+// 재귀를 통한 구현
+function strLength(arr){
+  if(arr[0] == null) return 0;
+  return strLength(arr.slice(0, -1)) + 1
+}
+
+let str = "abcde";
+let len = strLength(str);
+console.log(len);
